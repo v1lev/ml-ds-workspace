e6634ff995902c94a5c1bc5d5ab5e70606837a60
v1lev <vladi.lev.email@gmail.com>
2021-07-04 11:10:33 +0300

    add watermark and boosting libraries

--- scripts/conda_post_install.ps1
+++ scripts/conda_post_install.ps1
@@ -13,10 +13,11 @@ conda install ipywidgets -y
 jupyter nbextension enable --py widgetsnbextension 
 
 # libraries
-conda install catboost -y 
+conda install catboost xgboost lightgbm -y 
 conda install tensorflow-gpu -y 
-conda install scikit-learn-intelex -y 
+conda install scikit-learn-intelex -y
 
 # extensions
+conda install watermark -y
 conda install nb_conda -y 
 conda install nb_black -y 
