136c880156d20af617b968994f7f9423e843747e
v1lev <vladi.lev.email@gmail.com>
2021-07-07 13:24:55 +0300

    add extensions

--- scripts/conda_post_install.ps1
+++ scripts/conda_post_install.ps1
@@ -23,3 +23,15 @@ conda install nb_conda -y
 conda install nb_black -y
 conda install jupyterlab-git -y
 conda install jupyterlab_execute_time -y
+# gpu monitor
+conda install jupyterlab-nvdashboard -y
+conda install jupyterlab-variableinspector -y
+# matplotlib widget
+conda install ipympl -y
+
+# add environment for debugging
+conda create -n jupyterlab-debugger jupyterlab=3 xeus-python
+
+# not a conda extensions
+pip install aquirdturtle_collapsible_headings
+pip install jupyterlab-system-monitor
