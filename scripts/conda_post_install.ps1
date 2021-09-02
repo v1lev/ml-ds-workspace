09156ec5592eb851457824abe024d444ade9dc76
v1lev <vladi.lev.email@gmail.com>
2021-09-02 13:27:54 +0300

    add greedy downcastand libraries

--- scripts/conda_post_install.ps1
+++ scripts/conda_post_install.ps1
@@ -14,8 +14,11 @@ jupyter nbextension enable --py widgetsnbextension
 
 # libraries
 conda install catboost xgboost lightgbm -y 
-conda install tensorflow-gpu -y 
+conda install tensorflow-gpu=2.5 -y 
 conda install scikit-learn-intelex -y
+conda install cudatoolkit=11.3 -y
+conda install kaggle -y
+conda install opencv -y
 
 # extensions
 conda install watermark -y
@@ -23,15 +26,19 @@ conda install nb_conda -y
 conda install nb_black -y
 conda install jupyterlab-git -y
 conda install jupyterlab_execute_time -y
-# gpu monitor
-conda install jupyterlab-nvdashboard -y
-conda install jupyterlab-variableinspector -y
+# gpu monitor is really nice, but currently does not support windows
+# conda install jupyterlab-nvdashboard -y
+# not working properly too
+# conda install jupyterlab-variableinspector -y
 # matplotlib widget
 conda install ipympl -y
+conda install jupyter-resource-usage
 
 # add environment for debugging
-conda create -n jupyterlab-debugger jupyterlab=3 xeus-python
+conda create -n jupyterlab-debugger jupyterlab=3 xeus-python -y
+
+# environment for xgboost on GPU
+# conda create -n xgboost_env -c nvidia -c rapidsai py-xgboost cudatoolkit=11.3 ipykernel -y
 
 # not a conda extensions
 pip install aquirdturtle_collapsible_headings
-pip install jupyterlab-system-monitor
