20bf996785e28975d497163f6c4ab799c1a2d51b
v1lev <vladi.lev.email@gmail.com>
2021-09-02 14:27:20 +0300

    add plotly

---
+++ scripts/plotly.ps1
@@ -0,0 +1,9 @@
+conda install plotly
+# to render properly
+# https://github.com/jupyterlab/jupyter-renderers
+pip install jupyterlab-fasta
+pip install jupyterlab-geojson
+pip install jupyterlab-katex
+pip install jupyterlab-mathjax3
+pip install jupyterlab-vega2
+pip install jupyterlab-vega3
